ea069d6765446c793615ee8a307933e6b64f0b01
redwylie <mwylie@mgh.harvard.edu>
2020-03-31 16:52:27 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once('vendor/twig/twig/lib/Twig/Autoloader.php');
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('views');
+
+$twig = new Twig_Environment($loader, array(
+  'cache' => false,
+  'auto_reload' => true,
+));
+
+
+$file = file_get_contents('./images.json', true);
+
+//var_dump($file);
+
+$json = $file;
+
+$story = null;
+
+$images = json_decode($json);
+
+$categories = array();
+
+foreach ($images->{'images'} as &$image) {
+    if ($image->viewOnCategories) {
+        array_push($categories, $image);
+        $story = $images->story;
+    };
+}
+
+$template = $twig->loadTemplate('categories.html');
+echo $template->render(array(
+    'categories' => $categories,
+    'story' => $story,
+    'title' => 'MGH - Covid',
+	'description' => 'Covid Tutorials'
+));
+
+// $template = $twig->loadTemplate('index.html');
+// echo $template->render(array(
+//     'images' => $images->{'images'}
+// ));
+
+
+//include ("cards.php")
+
+?>
